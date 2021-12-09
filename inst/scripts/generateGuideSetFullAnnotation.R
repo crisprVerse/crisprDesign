ca2a19c774cdaa77dd5958ad8aa90882280cce24
fortinj2 <fortin.jeanphilippe@gene.com>
2021-12-09 15:47:22 -0800

    Regenerated rda files

--- inst/scripts/generateGuideSetFullAnnotation.R
+++ inst/scripts/generateGuideSetFullAnnotation.R
@@ -8,7 +8,7 @@ txObject <- txdb_human
 tssObject <- tss_human
 gr.repeats <- gr.repeats.hg38
 bowtie_index <- "~/crisprIndices/bowtie/hg38/hg38"
-vcf <- "~/crisprIndices/snps/dbsnp151.grch38/00-common_all_snps_only_maf1perc.vcf.gz"
+vcf <- "~/crisprIndices/snps/dbsnp151.grch38/00-common_all.vcf.gz"
 mart_dataset <- "hsapiens_gene_ensembl"
 
 gs <- guideSetExample
@@ -16,17 +16,25 @@ set.seed(46076919)
 indices <- sample(length(gs), 20)
 gs <- gs[indices]
 
+
 gs <- addSequenceFeatures(gs, addHairpin=TRUE)
-gs <- addSpacerAlignmentsIterative(gs, aligner='bowtie', txObject=txObject,
-                                   tssObject=tssObject, n_mismatches=3,
+gs <- addSpacerAlignmentsIterative(gs,
+                                   aligner='bowtie',
+                                   txObject=txObject,
+                                   tssObject=tssObject,
+                                   n_mismatches=3,
                                    bowtie_index=bowtie_index)
 gs <- addOffTargetScores(gs)
 gs <- addOnTargetScores(gs)
 gs <- addPamScores(gs)
-gs <- addRepeats(gs, gr.repeats=gr.repeats.hg38)
+gs <- addRepeats(gs,
+                 gr.repeats=gr.repeats.hg38)
 gs <- addRestrictionEnzymes(gs)
-gs <- addGeneAnnotation(gs, txObject=txObject, ignore_introns=FALSE, addPfam=TRUE, mart_dataset=mart_dataset)
-gs <- addTssAnnotation(gs, tssObject=tssObject)
+gs <- addGeneAnnotation(gs,
+                        txObject=txObject,
+                        ignore_introns=FALSE, addPfam=TRUE, mart_dataset=mart_dataset)
+gs <- addTssAnnotation(gs,
+                       tssObject=tssObject)
 gs <- addSNPAnnotation(gs, vcf=vcf)
 
 
