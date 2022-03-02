9ff6911cf1611672d93fe2e2174e0a47aa0be8bd
fortinj2 <fortin.jeanphilippe@gene.com>
2022-03-02 14:02:07 -0800

    Updated example

--- inst/examples/example_cas13d.R
+++ inst/examples/example_cas13d.R
@@ -12,17 +12,21 @@ out <- getMrnaSequences(txids,
                         bsgenome=bsgenome,
                         txObject=txObject)
 guides <- findSpacers(out[1], crisprNuclease=CasRx)
-guides <- guides[1:10]
+#guides <- guides[1:10]
 guides <- addOnTargetScores(guides)
 
 
-load("crisprDesignData/data/mrnasHuman.rda")
+
+load("../../../crisprDesignData/data/mrnasHuman.rda")
 guides <- addSpacerAlignments(guides,
                               n_mismatches=2,
                               aligner="biostrings",
                               both_strand=TRUE,
                               custom_seq=mrnasHuman[1:10])
 
+guides <- addSequenceFeatures(guides)
+guides <- addPamScores(guides)
+guides <- addRestrictionEnzymes(guides)
 
 
 
